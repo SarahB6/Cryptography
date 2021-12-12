a0418f9622cbe42b59d6b4a958485a6c0c8a2e40
sarahb6 <forsugar88@gmail.com>
2021-12-11 16:53:05 -0800

    Chapter 4

---
+++ CryptographyChap4/CryptographyChap4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CryptographyChap4
+{
+    class Program
+    {
+        public static string compress(string input)
+        {
+            return "";
+        }
+
+        public static string decompress(string input)
+        {
+            return "";
+        }
+
+        static void Main(string[]args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
