cfef4a56ba5e96e4144456fef208e4096fbddd37
sarahb6 <forsugar88@gmail.com>
2022-01-13 16:01:50 -0800

    Done with null

--- CryptographyChap7/CryptographyChap7/Program.cs
+++ CryptographyChap7/CryptographyChap7/Program.cs
@@ -4,60 +4,25 @@ namespace CryptographyChap7
 {
     class Program
     {
+
         public static string[,] encode(string input, int key)
         {
-            int numRows = (int)Math.Ceiling((double)input.Length/key);
+            int numRows = (int)Math.Ceiling((double)input.Length / key);
             int numCol = key;
             int count = 0;
-            bool hasAstrisk = false; // *
-            bool hasHash = false; // #
-            bool hasCaret = false; // ^
-            bool hasTilde = false; // ~
-            string[,] toReturn = new string[numRows,numCol];
-            for (int col = 0; col<numCol; col++)
+            string[,] toReturn = new string[numRows, numCol];
+            for (int col = 0; col < numCol; col++)
             {
-                for (int row = 0; row<numRows; row++)
+                for (int row = 0; row < numRows; row++)
                 {
-                    
+
                     if (count < input.Length)
                     {
-                        if (input[count].ToString() == "*")
-                        {
-                            hasAstrisk = true;
-                        }
-                        else if (input[count].ToString() == "#")
-                        {
-                            hasHash = true;
-                        }
-                        else if (input[count].ToString() == "^")
-                        {
-                            hasCaret = true;
-                        }
-                        else if (input[count].ToString() == "~")
-                        {
-                            hasTilde = true;
-                        }
                         toReturn[row, col] = input[count].ToString();
                         count++;
                     }
                     else
                     {
-                        if (!hasAstrisk)
-                        {
-                            toReturn[row, col] = "*";
-                        }
-                        else if (!hasHash)
-                        {
-                            toReturn[row, col] = "#";
-                        }
-                        else if (!hasCaret)
-                        {
-                            toReturn[row, col] = "^";
-                        }
-                        else
-                        {
-                            toReturn[row, col] = "~";
-                        }
                     }
 
                 }
@@ -72,6 +37,7 @@ namespace CryptographyChap7
                 string returning = "";
                 for (int col = 0; col < returned.GetLength(1); col++)
                 {
+                    if (returned[row, col] != null)
                     returning += $"{returned[row, col]}";
                 }
                 toReturn += $"{returning}";
@@ -86,40 +52,16 @@ namespace CryptographyChap7
             {
                 for (int row = 0; row < coded.GetLength(0); row++)
                 {
-                    //testing
-                    if (coded.GetLength(1) - 1 == col)
-                    {
-                        if (coded[row, col] == "*" && row != coded.GetLength(0) - 1 ||
-                            coded[row, col] == "#" && row != coded.GetLength(0) - 1 ||
-                            coded[row, col] == "^" && row != coded.GetLength(0) - 1 ||
-                            coded[row, col] == "~" && row != coded.GetLength(0) - 1)
-                        {
-                            if (coded[row + 1, col] == "*" ||
-                                coded[row + 1, col] == "#" ||
-                                coded[row + 1, col] == "^" ||
-                                coded[row + 1, col] == "~")
-                            { }
-                        }
-                        else if (coded[row, col] == "*" && row == coded.GetLength(0) - 1 ||
-                                 coded[row, col] == "#" && row == coded.GetLength(0) - 1 ||
-                                 coded[row, col] == "^" && row == coded.GetLength(0) - 1 ||
-                                 coded[row, col] == "~" && row == coded.GetLength(0) - 1)
-                        { }
-                        else
-                        {
-                            //works - special characters
-                            toReturn += coded[row, col];
-                        }
-                    }
-                    else
+                    if(coded[row, col] != null)
                     {
                         toReturn += coded[row, col];
                     }
-                    
+
                 }
             }
             return toReturn;
         }
+
         static void Main(string[] args)
         {
             string input = "check if it worked";
